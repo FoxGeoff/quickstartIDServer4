eacaf90eb25b62046f4c5231fb28fe12e28623bb
FoxGeoff <fox.geoffrey@gmail.com>
2019-09-19 16:23:43 -0700

    Defining the client

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -27,7 +27,25 @@ namespace IdentityServer
 
         public static IEnumerable<Client> GetClients()
         {
-            return new Client[] { };
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
         }
     }
 }
